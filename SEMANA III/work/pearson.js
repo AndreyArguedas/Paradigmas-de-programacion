5f0f8fc44de088c9ba1966ad409e66b954c48ad6
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-08-06 21:42:15 -0600

    Pearson 2

--- SEMANA III/work/pearson.js
+++ SEMANA III/work/pearson.js
@@ -1,23 +1,20 @@
 
 function pearson(x, y){
 	var quant = (a, b) => a.length + b.length;
+	var desv = (a) => sumPow(a) / a.length;
 	var n = quant(x, y);
 	var sumx = sumPow(x);
 	var sumy = sumPow(y);
 	var sum2 = sumxy(x, y);
 	var sumPowx = sumPow(x,2);
 	var sumPowy = sumPow(y,2);
-	return (sum2 - n * sumx * sumy) / 
-			(Math.sqrt(sumPowx - n) * Math.sqrt(sumPowy - n));
+	return (sum2 -  n * sumx * sumy) / (Math.sqrt((sumPowx - n * Math.pow(desv(x),2))) * Math.sqrt((sumPowy - n * Math.pow(desv(y),2)))) ;
 }
 
 function sumxy(x, y){
-	return x.reduce( (acc, curr, i) => acc + curr + y[i], 0 );
+	return x.reduce( (acc, curr, i) => acc + (curr * y[i]), 0 );
 }
 
-/*function sum(a){
-	return a.reduce( (acc, curr) => acc + curr, 0);
-}*/
 
 function sumPow(a, exp = 1){
 	return a.reduce( (acc, curr) => acc + Math.pow(curr,exp), 0 );
