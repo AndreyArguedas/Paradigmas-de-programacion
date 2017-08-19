0b0f7b690e0645e7430379633d9fecbd4f2577f6
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-08-19 12:58:34 -0600

    About this

---
+++ SEMANA IV/About this/about.js
@@ -0,0 +1,15 @@
+function Foo(x){
+	this.x = x;
+}
+
+var foo = new Foo(6); //Aqui apunta al this de Foo
+var wtf = Foo(9); //Aqui apunta al this de global/window
+
+var obj = {x : 0};
+var foo2 = Foo.call(obj, 777); //Aqui falta new
+console.log(foo2, obj); //Foo2 da indefinido por la falta de new arriba
+
+var updater = Foo.bind(obj); //Si la llaman actualiza obj
+updater(99);
+console.log(obj);
+console.log(updater);
