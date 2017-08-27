ecf44e057ef2e541aa9dd3036f33fd830b859036
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-08-27 16:49:45 -0600

    Generadores intro

---
+++ SEMANA V/Introduccion a generadores/generators.js
@@ -0,0 +1,33 @@
+function * generator() {
+	let i = 0;
+	while(true){
+		yield Math.pow(i,10);
+		i++;
+	}
+}
+
+let iterator = generator();
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+
+function *composeGenerator() {
+	yield 1;
+	yield * anotherGenerator();
+	//A return here will stop it
+	yield 3;
+}
+
+function * anotherGenerator() {
+	yield 2;
+}
+
+let iterator = composeGenerator();
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
