2ea39e9387d888b655e4af63b1d813016db87a9e
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-10-24 00:20:08 -0600

    Practica kotlin intro 01 hecha

--- SEMANA X/FP_KT_INTRO1/work/ejercicio1.kt
+++ SEMANA X/FP_KT_INTRO1/work/ejercicio1.kt
@@ -2,8 +2,11 @@ fun printOnlyNumbers(args: Array<String>){
     println("Print only numbers")
     for((index,value) in args.withIndex()){
         val num = value.toIntOrNull()
-        if(num != null)
-            println("$index) num=$num")
+        if(num != null){
+            val inr = inRange(1, 10, num)
+            println("$index) num=$num inRange(1, 10)=$inr")
+        }
+            
         else
             println("*** $value ***")
     }
@@ -17,6 +20,18 @@ fun maxMinOfList(nums: List<Int>): List<Int>{
     return listOf(nums.min()!!, nums.max()!!)
 }
 
+fun inRange(min : Int, max : Int, value : Int) : Boolean{
+    return value in min..max 
+}
+
+fun gcd(a : Int, b : Int) : Int{
+    return when{
+        a % b == 0 ->  b;
+        a > b ->  gcd(a - b, b)
+        else ->  gcd(b , a)
+    }
+} 
+
 fun main(args: Array<String>){
 	println(">>> Testing <<<")
 	var n = 10
@@ -24,4 +39,6 @@ fun main(args: Array<String>){
     printOnlyNumbers(args)
     val maxMin = maxMinOfList(selectOnlyNums(args))
     println("max=${maxMin[1]} min=${maxMin[0]}")
+    val gcdval = gcd(maxMin[1],maxMin[0])
+    println("gcd=$gcdval")
 }
