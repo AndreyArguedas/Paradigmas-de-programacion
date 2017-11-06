0ab8906f385fa1e6c6b555587b8fa9f94e9b9a01
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-11-05 21:23:11 -0600

    Calc done

--- SEMANA XIII/Practica_KT-CALC/work/src/calc.kt
+++ SEMANA XIII/Practica_KT-CALC/work/src/calc.kt
@@ -8,7 +8,9 @@ package eif400.calc;
 sealed class Expr
 class Num( val value: Double ) : Expr()
 class Add( vararg val args: Expr ) : Expr()
+class Minus( vararg val args: Expr ) : Expr()
 class Times( vararg val args: Expr ) : Expr()
+class Div( vararg val args: Expr ) : Expr()
 //... add more here if needed
 object NullExpr : Expr(){
    override fun toString()="nullExpr"
@@ -17,10 +19,15 @@ object NullExpr : Expr(){
 fun eval(expr: Expr): Double =
    when (expr){
      is Num   -> expr.value
-	 is Add   -> expr.args.fold(0.0, {sum, e -> sum + eval(e)})
+	   is Add   -> expr.args.fold(0.0, {sum, e -> sum + eval(e)})
+     is Minus -> expr.args.fold(0.0, {min, e -> 
+       if( min == 0.0) eval(e) else  min - eval(e)
+       })
      is Times -> expr.args.fold(1.0, {mul, e -> mul * eval(e)})
-	 //... add more here
-	 is NullExpr -> Double.NaN
+     is Div   -> expr.args.fold(1.0, {div, e -> 
+        if( div == 1.0) eval(e) else  div / eval(e)
+    })
+	   is NullExpr -> Double.NaN
    }
 fun test1(){
    println("test1: (666 + 999) * 100")
@@ -38,7 +45,8 @@ fun test2(){
    val num2 = Num(999.0)
    val num3 = Num(100.0)
    //... modify to make it work
-   val res = NullExpr
+   val expr = Div( Minus( num1, num2 ), num3 )
+   val res = eval(expr)
    val expected = (num1.value - num2.value) / num3.value
    println("test2: eval --> $res (expected=$expected)")
 }
